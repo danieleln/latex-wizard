dcf29ad7cbf6f5a1fa672c9cb1c53f72e6e8fbee
danieleln <daniele.monzani@outlook.com>
2024-08-01 11:06:31 +0200

    feat(compile): run pdflatex, makeglossaries, biber

--- src/subcommands/compile.rs
+++ src/subcommands/compile.rs
@@ -1,17 +1,22 @@
-use crate::config::structure::MAIN_TEX_FILE;
+use crate::config::structure::{MAIN_TEX_FILE, OUTPUT_DIRECTORY};
 use crate::logs::{log_info, Log};
 use clap::ArgMatches;
 use std::env;
+use std::fs;
 use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
 
 pub fn compile(args: &ArgMatches) -> Result<(), Log> {
     let proj_name = args.get_one::<String>("project");
 
     // Look for the main `.tex` file to compile
     let main_tex_file = find_main_tex_file(&proj_name)?;
-    log_info(format!("Compiling `{}`", main_tex_file.display()));
 
-    Ok(())
+    // Find the output directory of the project
+    let proj_dir = main_tex_file.parent().unwrap();
+    let output_directory = proj_dir.join(OUTPUT_DIRECTORY);
+
+    compile_tex_file(&main_tex_file, &output_directory)
 }
 
 pub fn find_main_tex_file(proj_name: &Option<&String>) -> Result<PathBuf, Log> {
@@ -90,4 +95,133 @@ pub fn find_main_tex_file(proj_name: &Option<&String>) -> Result<PathBuf, Log> {
             }
         }
     }.map_err(|e| Log::InvalidCommandLineArgument(e))
+}
+
+// Compile a tex file
+pub fn compile_tex_file(tex_file: &PathBuf, output_directory: &PathBuf) -> Result<(), Log> {
+    // Create the output directory if it's missing
+    if !output_directory.exists() {
+        fs::create_dir(output_directory).map_err(|e| {
+            Log::FileSystemError(format!(
+                "While creating output directory `{}`:\n{}",
+                output_directory.display(),
+                e.to_string()
+            ))
+        })?;
+    }
+
+    // Log some infos to the user
+    log_info(format!(
+        "Compiling `{}` into `{}`",
+        tex_file.display(),
+        output_directory.display()
+    ));
+
+    // Compilation process:
+    // 1. run `pdflatex` a first time
+    // 2. compile glossaries using `makeglossaries` command
+    // 3. compile biblography using the `biber` command
+    // 4. run `pdflatex` a second time
+
+    // 1. Run pdflatex
+    let output = Command::new("pdflatex")
+        .arg("-halt-on-error")
+        .arg("-output-directory")
+        .arg(output_directory)
+        .arg(tex_file)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .map_err(|e| {
+            Log::ShellCommandError(format!(
+                "Failed to spawn command `pdflatex`:\n{}",
+                e.to_string()
+            ))
+        })?;
+
+    if !output.status.success() {
+        return Err(Log::ShellCommandError(format!(
+            "Command `pdflatex` failed with exit code {}.\n{}\n{}",
+            output.status.code().unwrap_or(-1),
+            String::from_utf8_lossy(&output.stdout),
+            String::from_utf8_lossy(&output.stderr)
+        )));
+    }
+
+    // 2. Run makeglossaries
+    let output = Command::new("makeglossaries")
+        .arg("-d")
+        .arg(output_directory)
+        .arg(tex_file.file_stem().unwrap())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .map_err(|e| {
+            Log::ShellCommandError(format!(
+                "Failed to spawn command `makeglossaries`:\n{}",
+                e.to_string()
+            ))
+        })?;
+
+    if !output.status.success() {
+        return Err(Log::ShellCommandError(format!(
+            "Command `makeglossaries` failed with exit code {}.\n{}\n{}",
+            output.status.code().unwrap_or(-1),
+            String::from_utf8_lossy(&output.stdout),
+            String::from_utf8_lossy(&output.stderr)
+        )));
+    }
+
+    // Run biber
+    let output = Command::new("biber")
+        .arg("--input-directory")
+        .arg(output_directory)
+        .arg("--output-directory")
+        .arg(output_directory)
+        .arg(tex_file.file_stem().unwrap())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .map_err(|e| {
+            Log::ShellCommandError(format!(
+                "Failed to spawn command `biber`:\n{}",
+                e.to_string()
+            ))
+        })?;
+
+    if !output.status.success() {
+        return Err(Log::ShellCommandError(format!(
+            "Command `biber` failed with exit code {}.\n{}\n{}",
+            output.status.code().unwrap_or(-1),
+            String::from_utf8_lossy(&output.stdout),
+            String::from_utf8_lossy(&output.stderr)
+        )));
+    }
+
+    // 4. Run pdflatex
+    let output = Command::new("pdflatex")
+        .arg("-halt-on-error")
+        .arg("-output-directory")
+        .arg(output_directory)
+        .arg(tex_file)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .map_err(|e| {
+            Log::ShellCommandError(format!(
+                "Failed to spawn command `pdflatex`:\n{}",
+                e.to_string()
+            ))
+        })?;
+
+    if !output.status.success() {
+        return Err(Log::ShellCommandError(format!(
+            "Command `pdflatex` failed with exit code {}.\n{}\n{}",
+            output.status.code().unwrap_or(-1),
+            String::from_utf8_lossy(&output.stdout),
+            String::from_utf8_lossy(&output.stderr)
+        )));
+    }
+
+    Ok(())
 }
