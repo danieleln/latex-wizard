b3720f176bb66749016194fbedc81d6f87a2439d
danieleln <daniele.monzani@outlook.com>
2024-08-02 10:57:51 +0200

    feat(init): add .gitignore file

--- src/subcommands/init.rs
+++ src/subcommands/init.rs
@@ -2,6 +2,7 @@ use crate::config::structure::{MAIN_TEX_FILE, OUTPUT_DIRECTORY};
 use crate::logs::Log;
 use clap::ArgMatches;
 use std::fs;
+use std::io::Write;
 use std::process::Command;
 
 pub fn init(args: &ArgMatches) -> Result<(), Log> {
@@ -36,6 +37,30 @@ pub fn init(args: &ArgMatches) -> Result<(), Log> {
         ))
     })?;
     // TODO: write a template latex code to the main `.tex` file
+
+    // Create a .gitignore file
+    let mut gitignore =
+        fs::File::create(format!("{}/{}", proj_name, ".gitignore")).map_err(|e| {
+            Log::FileSystemError(format!(
+                "While creating file `{}/{}`:\n{}",
+                proj_name,
+                ".gitignore",
+                e.to_string()
+            ))
+        })?;
+
+    // Copy the template .gitignore file
+    const GITIGNORE_TEMPLATE: &str = include_str!("../templates/gitignore");
+    gitignore
+        .write_all(GITIGNORE_TEMPLATE.as_bytes())
+        .map_err(|e| {
+            Log::FileSystemError(format!(
+                "While writing to `{}/{}`:\n{}",
+                proj_name,
+                ".gitignore",
+                e.to_string()
+            ))
+        })?;
 
     // Run "git init" command
     let output = Command::new("git")
